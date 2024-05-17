388c41aa5ee879372c2f08f2f7c13f89c7e5db26
blckngel696 <digitaltraveler666@gmail.com>
2024-05-17 20:30:01 +0600

    v0.4.0

--- src/crud/user/user.module.ts
+++ src/crud/user/user.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Users } from '../../database/schema/user.entity'; 
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
   controllers: [UserController],
+  imports: [
+    DatabaseModule,
+    TypeOrmModule.forFeature([Users]),
+  ],
   providers: [UserService]
 })
 export class UserModule {}
--- src/crud/user/user.service.ts
+++ src/crud/user/user.service.ts
@@ -1,6 +1,14 @@
 import { Injectable } from '@nestjs/common';
-import { Users } from '../../database/schema/user.entity';
+import { Users } from '../../database/schema/user.entity'; // Исправленный путь импорта
 import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(Users) // Исправлено Users на User
+    private readonly userRepository: Repository<Users>
+  ) {}
+
+  // Добавьте остальные методы и логику сервиса здесь
+}
