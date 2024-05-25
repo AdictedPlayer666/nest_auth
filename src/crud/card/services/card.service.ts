2d7e01f97559bcf9453cc167689ad42e5b05298f
blckngel696 <digitaltraveler666@gmail.com>
2024-05-25 22:18:27 +0600

    Some fixes

--- src/crud/card/services/card.service.ts
+++ src/crud/card/services/card.service.ts
@@ -31,14 +31,14 @@ export class CardService {
 
         const newCard = this.cardRepository.create({ user_id, card_name, column_id });
         const createdCard = await this.cardRepository.save(newCard);
-        
+
         if (createdCard) {
             return { message: 'Card created successfully' };
         }
         throw new BadRequestException("Create error");
     }
     async getCard(cardDto: CardDto): Promise<string> {
-        
+
         const cardExisted = await this.cardRepository.findOne({ where: { user_id: cardDto.id, column_id: cardDto.column_name, card_name: cardDto.card_name } });
         if (!cardExisted) {
             throw new BadRequestException("Card not founded");
@@ -55,7 +55,7 @@ export class CardService {
 
     async deleteCard(cardDto: CardDto): Promise<any> {
 
-  
+
         const column = await this.columnRepository.findOne({ where: { user_id: cardDto.id, column_name: cardDto.column_name } });
         const column_id = column?.column_id;
         const cardExisted = await this.cardRepository.findOne({ where: { user_id: cardDto.id, column_id: cardDto.column_name, card_name: cardDto.card_name } });
@@ -63,7 +63,7 @@ export class CardService {
         if (!cardExisted) {
             throw new NotFoundException("card not founded");
         }
-        
+
         const deletedColumn = await this.cardRepository.delete({ user_id: cardDto.id, column_id, card_name: cardDto.card_name });
         if (!!deletedColumn.affected) {
             return { message: 'Card deleted successfully' };
